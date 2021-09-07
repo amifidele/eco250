396a4cfab023783dc81bf5ee931e67120563bf3e
amifidele <amifidele333@gmail.com>
2021-09-07 21:23:12 +0300

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="{{  }}">
+
+        <title>Eco250.rw</title>
+
+        <style>
+          
+        </style>
+    </head>
+    <body>
+
+        <div class="w-100">
+
+        </div>
+        
+    </body>
+</html>
